27e1eb8e70bdd05b8d642793ee09b09241e9ee8d
Mahesh Prasad <maheshprasad2422005@gmail.com>
2026-07-29 22:14:14 +0530

    Time: 2 ms (46.34%), Space: 43.2 MB (58.04%) - LeetHub

---
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+
+    public int countStudents(int[] students, int[] sandwiches) {
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int student : students) {
+            queue.offer(student);
+        }
+
+        int index = 0;
+        int count = 0;
+
+        while (!queue.isEmpty()) {
+
+            if (queue.peek() == sandwiches[index]) {
+
+                queue.poll();
+                index++;
+                count = 0;
+
+            } else {
+
+                queue.offer(queue.poll());
+                count++;
+
+                if (count == queue.size()) {
+                    break;
+                }
+            }
+        }
+
+        return queue.size();
+    }
+}
